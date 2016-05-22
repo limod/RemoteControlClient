cee6155f4b6bb84e6f66fe11e46ebd7da6716048
Dominic Lehner <dominic.lehner@arcor.de>
2016-05-22 17:39:46 +0200

    Spotify AsyncTask implementiert

--- app/src/main/java/limod/de/remotecontrolclient/MainActivity.java
+++ app/src/main/java/limod/de/remotecontrolclient/MainActivity.java
@@ -1,15 +1,31 @@
 package limod.de.remotecontrolclient;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import limod.de.remotecontrolclient.services.SpotifyTask;
+import org.glassfish.jersey.client.ClientConfig;
+
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AppCompatActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +42,35 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                new SpotifyTask().execute(SpotifyTask.Action.PLAY);
+
+            }
+        });
+
+        Button btnPause = (Button) findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                new SpotifyTask().execute(SpotifyTask.Action.PAUSE);
+
+            }
+        });
+
+        Button btnNext= (Button) findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                new SpotifyTask().execute(SpotifyTask.Action.NEXT);
+
+            }
+        });
+
+
     }
 
     @Override
@@ -48,5 +93,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class RetrieveFeedTask extends AsyncTask<Void, Void, Void> {
+
+        private Exception exception;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            ClientConfig clientConfig = new ClientConfig();
+            Client client = ClientBuilder.newClient(clientConfig);
+
+            WebTarget webTarget = client.target("http://192.168.1.11:8080/myapp");
+            WebTarget vlcResource = webTarget.path("spotify");
+            WebTarget vlcTest = vlcResource.path("pause");
+
+            Invocation.Builder request = vlcTest.request(MediaType.APPLICATION_JSON_TYPE);
+            try {
+                Response response = request.get();
+                System.out.println(response.getStatus());
+                System.out.println(response.readEntity(String.class));
+            } catch (ProcessingException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            System.out.println("post execute");
+            Log.d(MainActivity.TAG, "post execute");
+
+        }
     }
 }
